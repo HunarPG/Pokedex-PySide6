daccacd1f5d3d2fd6fee6b250320af2477bbd7ea
HunarPG <HunarSangwanPG@gmail.com>
2024-04-27 20:42:16 +0530

    Added an about QT Window

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -1,10 +1,17 @@
 from ui.mainwindow_ui import Ui_MainWindow
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import QMainWindow, QMessageBox
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
+
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
         self.setWindowTitle("Pokedex")
+
+        self.ui.actionAbout_Qt.triggered.connect(self.show_about_qt)
+
+    def show_about_qt(self):
+        QMessageBox.aboutQt(self)
