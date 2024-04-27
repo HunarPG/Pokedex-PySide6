81f0e318eb9e2fb5f7be0ab8856be6825751eaf9
HunarPG <HunarSangwanPG@gmail.com>
2024-04-27 21:59:30 +0530

    Added pokemon, ability and move names to combo boxes

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -1,6 +1,27 @@
 from ui.mainwindow_ui import Ui_MainWindow
 from PySide6.QtWidgets import QMainWindow, QMessageBox
 
+pokemon_names = []
+ability_names = []
+move_names = []
+
+with open("database/pokemon.txt", "r") as pokemon_database:
+    num_pokemon = 0
+    while num_pokemon<1025:
+        pokemon_names.append(pokemon_database.readline())
+        num_pokemon+=1
+
+with open("database/ability.txt", "r") as ability_database:
+    num_ability = 0
+    while num_ability<310:
+        ability_names.append(ability_database.readline())
+        num_ability+=1
+
+with open("database/move.txt", "r") as move_database:
+    num_move = 0
+    while num_move<934:
+        move_names.append(move_database.readline())
+        num_move+=1
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -13,6 +34,10 @@ class MainWindow(QMainWindow):
 
         self.ui.actionAbout_Qt.triggered.connect(self.show_about_qt)
         self.ui.actionAbout_Pokedex.triggered.connect(self.show_about_pokedex)
+
+        self.ui.comboBox_Pokemon.addItems(pokemon_names)
+        self.ui.combobox_Ability.addItems(ability_names)
+        self.ui.comboBox_Move.addItems(move_names)
 
     def show_about_qt(self):
         dialog = QMessageBox()
