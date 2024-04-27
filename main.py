2f9c428a293e77468b1caabaad3056c978ad062d
HunarPG <HunarSangwanPG@gmail.com>
2024-04-27 20:22:36 +0530

    Started PySide6 project

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow
+from ui.ui_mainwindow import Ui_MainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.setWindowTitle("Pokedex")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
