d8b379a8f2d6940e0697a35fc72a2998cb94f420
HunarPG <HunarSangwanPG@gmail.com>
2024-04-29 17:01:34 +0530

    Replaced images with images from Serebii(transparent)

--- scripts/Downloading Pokemon Images.py
+++ scripts/Downloading Pokemon Images.py
@@ -1,12 +1,37 @@
 import requests
 
-with open("../database/pokemon.txt", "r") as pokemon_database:
-    for pokemon_name in pokemon_database:
-        pokemon_name = pokemon_name.strip("\n")
-        downloadurl = f'https://img.pokemondb.net/artwork/{pokemon_name}.jpg'
-        req = requests.get(downloadurl)
-        filename = f'../img/{pokemon_name}.jpg'
-        with open(filename, 'wb') as f:
-            for chunk in req.iter_content(chunk_size=8192):
-                if chunk:
-                    f.write(chunk)
+# For Pokemon 0-9
+# pokemon_number = 1
+# while(pokemon_number<=9):
+#     downloadurl = f"https://www.serebii.net/pokemon/art/00{pokemon_number}.png"
+#     req = requests.get(downloadurl)
+#     filename = f"img/00{pokemon_number}.png"
+#     with open(filename, "wb") as f:
+#         for chunk in req.iter_content(chunk_size=8192):
+#             if chunk:
+#                 f.write(chunk)
+#     pokemon_number += 1
+
+# For Pokemon 10-99
+# pokemon_number = 10
+# while(pokemon_number<=99):
+#     downloadurl = f"https://www.serebii.net/pokemon/art/0{pokemon_number}.png"
+#     req = requests.get(downloadurl)
+#     filename = f"img/0{pokemon_number}.png"
+#     with open(filename, "wb") as f:
+#         for chunk in req.iter_content(chunk_size=8192):
+#             if chunk:
+#                 f.write(chunk)
+#     pokemon_number += 1
+
+# For Pokemon 100-1025
+pokemon_number = 100
+while(pokemon_number<=1025):
+    downloadurl = f"https://www.serebii.net/pokemon/art/{pokemon_number}.png"
+    req = requests.get(downloadurl)
+    filename = f"img/{pokemon_number}.png"
+    with open(filename, "wb") as f:
+        for chunk in req.iter_content(chunk_size=8192):
+            if chunk:
+                f.write(chunk)
+    pokemon_number += 1
