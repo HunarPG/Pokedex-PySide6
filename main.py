36cccb228f91cd7513ca970b4214190c15cc2ad2
HunarPG <HunarSangwanPG@gmail.com>
2024-04-30 19:29:40 +0530

    Removed unnecessary Qt Creator stuff from Main File

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import sys
 from PySide6.QtWidgets import QApplication
 from src.MainWindow import MainWindow
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec())
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
